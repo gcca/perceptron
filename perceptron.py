484c9987c6dc5c9265252c746403628b290a6122
gcca <gcca@gcca.tk>
2011-08-06 17:50:03 -0500

    perceptron.py

---
+++ perceptron.py
@@ -0,0 +1,63 @@
+class Perceptron:
+	'''
+	Neurona :Perceptron
+
+	:Parametros:
+		pesos: vector de decimal
+				Pesos de la neurona
+		umbral: decimal
+				Umbral de activacion
+
+	:Formato:
+		Ejemplo 1: para 2 criterios de entradas
+			>>> 
+				...
+				
+	'''
+	def __init__(self, NEntradas):
+      pass
+  
+	def suma(self, entradas):
+		'''
+		Suma de las entradas ponderadas por los pesos
+
+		:Parametros:
+			entradas: vector
+				Vector de entradas
+
+		:Retorna:
+			salida: decimal
+				Suma ponderada
+
+		'''
+
+		pass
+
+	def calcular(self, entradas):
+		'''
+		Calculo de activacion de la neurona
+
+		:Parametros:
+			entradas: vector
+				Vector de entradas
+
+		:Retorna:
+			salida: bool en {0, 1}
+				0 si desactivado
+				1 si activado
+				
+		'''
+		
+		pass
+
+	def entrenar(self, conjuntoEntrenamiento):
+		'''
+		Entrenamiento de la neurona
+
+		:Parametros:
+			conjuntoEntrenamiento: vector
+				Tuplas de entradas para el entrenamiento
+				
+		'''
+		
+    pass
