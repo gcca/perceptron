64fc610c17cd180b7bcafe484094e996086a8b7f
gcca <gcca@gcca.tk>
2011-08-06 17:53:03 -0500

    imgpng.py

---
+++ imgpng.py
@@ -0,0 +1,19 @@
+class ImgPNG:
+	'''
+	Imagen PNG
+
+	:Parametros:
+		fichero: cadena
+			Nombre del fichero de imagen PNG
+	
+	'''
+	def __init__(self, fichero = None):
+    pass
+
+	def decodificar(self):
+		'''
+		Decodificar la imagen PNG a vector binario
+		
+		'''
+		
+		pass
