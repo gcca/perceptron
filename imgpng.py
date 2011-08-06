20280d02f195ddeefb278c69d1b08fca087d7b4a
gcca <gcca@gcca.tk>
2011-08-06 18:05:10 -0500

    imgpng.py - +decodificar

--- imgpng.py
+++ imgpng.py
@@ -18,4 +18,13 @@ class ImgPNG:
 		
 		'''
 		
-		pass
+		if not self.vector:
+			self.img = self.png.read()
+			self.imgBin = list(self.img[2])
+
+			for i in xrange(len(self.imgBin)/20):
+				for j in xrange(len(self.imgBin[0])/(20*4)):
+					self.vector.append(0 if self.imgBin[10+20*i][40+4*20*j] else 1)
+			self.fichero.close()
+			
+		return self.vector
